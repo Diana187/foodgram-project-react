1f4aaffaf6cd84ee06ba09fec9ec0c3950fcd3b6
Диана Амирханова <dinaami187@gmail.com>
2023-04-23 19:31:49 +0100

    edited filters.py, views.py: added backend_filters

--- backend/api/views.py
+++ backend/api/views.py
@@ -22,40 +22,15 @@ class IngredientApiViewSet(viewsets.ReadOnlyModelViewSet):
     # search_fields = ('^name', )
 
 
-class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
-    filter_backends = (IngredientFilter, )
-    search_fields = ('name', )
-
-
-class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
-    """ViewSet для модели Ingredient.
-    Предоставляет возможности получения списка
-    и детальной информации об ингредиентах."""
-
-    filter_backends = (CustomSearchFilter,)
-    search_fields = ("^name",)
+class TagApiViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    pagination_class = RecipePagination
+    permission_classes = (IsAuthenticatedOrReadOnly, )
 
 
 class RecipeApiViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     pagination_class = RecipePagination
-
-
-class TagApiViewSet(viewsets.ModelViewSet):
-    queryset = Tag.objects.all()
-    serializer_class = TagSerializer
-
-
-class FavoriteApiViewSet(viewsets.ModelViewSet):
-    queryset = Favorite.objects.all()
-    pass
-
-class RecipeIngredientAmountApiViewSet(viewsets.ModelViewSet):
-    queryset = RecipeIngredientAmount.objects.all()
-    pass
-
-
-class ShoppingListViewSet(viewsets.ModelViewSet):
-    queryset = ShoppingList.objects.all()
-    pass
+    filter_backends = (RecipeFilter, )
