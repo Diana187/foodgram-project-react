db15d9f11f190183750e72a7c40ad71d3923048b
Диана Амирханова <dinaami187@gmail.com>
2023-06-13 13:56:54 +0100

    добавила пагинатор в кастомном пагинаторе

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -3,6 +3,6 @@ from rest_framework.pagination import PageNumberPagination
 
 class RecipePagination(PageNumberPagination):
     """Пагинатор для определения количества элементов на странице."""
-    page_size = 6
+    page_size = 15
     page_size_query_param = 'page_size'
     max_page_size = 15
